0745c1fc14c4e295c097417aa36ae8fcd5a52dbc
Star Bist <malimirkeccita@gmail.com>
2016-10-23 13:26:31 +0200

    Adding accessability.

--- TwitchSwitch.js
+++ TwitchSwitch.js
@@ -1,5 +1,3 @@
-var labels = document.querySelectorAll('.tabs__label');
-
 var hideAll = function() {
     var contentElements = document.querySelectorAll('.tabs__content');
 
@@ -8,22 +6,55 @@ var hideAll = function() {
     }
 };
 
+var resetTabindexAll = function() {
+    var contentElements = document.querySelectorAll('.tabs__content');
+
+    for (var i = 0; i < contentElements.length; i++) {
+        contentElements[i].setAttribute('tabindex', -1);
+    }
+};
+
 var unselectAll = function() {
     var contentLabels = document.querySelectorAll('.tabs__label');
 
     for (var i = 0; i < contentLabels.length; i++) {
         contentLabels[i].setAttribute('aria-selected', false);
+        contentLabels[i].setAttribute('aria-expanded', false);
     }
 };
 
-var TwitchSwitch = function(e) {
+var TwitchSwitchRadio = function(e) {
     hideAll();
     unselectAll();
 
-    document.querySelector('[aria-labelledby="' + this.id + '"]').setAttribute('aria-hidden', false);
-    this.setAttribute('aria-selected', true);
+    var labelId = 'tablabel' + this.value;
+    var panel = document.querySelector('[role="tablist"]');
+    var label = document.querySelector('#' + labelId);
+    var content = document.querySelector('[aria-labelledby="' + label.id + '"]');
+    panel.setAttribute('aria-activedescendant', labelId);
+    content.setAttribute('aria-hidden', false);
+    label.setAttribute('aria-selected', true);
+    label.setAttribute('aria-expanded', true);
 };
 
-for (var i = 0; i < labels.length; i++) {
-    labels[i].addEventListener('click', TwitchSwitch);
+var TwitchSwitchLabel = function(e) {
+    if(e.keyCode === 13) {
+        resetTabindexAll();
+
+        var labelId = 'tablabel' + this.value;
+        var label = document.querySelector('#' + labelId);
+        var content = document.querySelector('[aria-labelledby="' + label.id + '"]');
+        content.setAttribute('tabindex', 0);
+        content.focus();
+    }
+};
+
+var radios = document.querySelectorAll('.tabs__radio');
+
+for (var i = 0; i < radios.length; i++) {
+    radios[i].addEventListener('change', TwitchSwitchRadio);
+}
+
+for (var i = 0; i < radios.length; i++) {
+    radios[i].addEventListener('keypress', TwitchSwitchLabel);
 }
